061ed2bd1d41b371851179395b7e05892eddf048
Yiğit Gürel Mora <106966992+YigitMora@users.noreply.github.com>
2026-02-26 13:32:33 +0300

    Create myFirstLabScript.cpp

---
+++ LAB1/myFirstLabScript.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Hello " << name << "." << endl;
+
+    string studentId;
+    cout << "What is your Student ID? ";
+    cin >> studentId;
+    cout << "Your ID is " << studentId << "." << endl;
+
+    return 0;
+}
